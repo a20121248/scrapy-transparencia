238d280512ed916f588f14f52e97924da4d1e100
Javier Monzon <javiermd13@gmail.com>
2021-07-11 21:07:23 -0500

    cambios

--- transparencia_v2/pipelines.py
+++ transparencia_v2/pipelines.py
@@ -13,24 +13,32 @@ class csvWriterPipeline(object):
 
     def __init__(self) -> None:
         self.items_written_infogeneral = 0
-        self.data_encontrada_infogeneral = []
+        self.columnsPrincipal = ['tipo_poder_id','tipo_poder_nombre','categoria','entidad_id','entidad_nombre','pk_id_personal','vc_personal_ruc_entidad','in_personal_anno','in_personal_mes','vc_personal_regimen_laboral','vc_personal_paterno','vc_personal_materno','vc_personal_nombres','vc_personal_cargo','vc_personal_dependencia','mo_personal_remuneraciones','mo_personal_honorarios','mo_personal_incentivo','mo_personal_gratificacion','mo_personal_otros_beneficios','mo_personal_total','vc_personal_observaciones','fec_reg']
+        self.data_encontrada_infogeneral = pd.DataFrame(columns = self.columnsPrincipal)
         self.ctd_save_infogeneral = 2
         self.prename_infogeneral = ''
-        self.columnsPrincipal = ''
         pass
 
     def process_item(self, item, spider):
         if isinstance(item, InformacionEntidadItem):
             self.prename_infogeneral = f'{self.out_path}{YYYYMMDD_HHMMSS}_informacion_personal.txt'
-            self.columnsPrincipal = ['tipo_poder_id','tipo_poder_nombre','categoria','entidad_id','entidad_nombre','pk_id_personal','vc_personal_ruc_entidad','in_personal_anno','in_personal_mes','vc_personal_regimen_laboral','vc_personal_paterno','vc_personal_materno','vc_personal_nombres','vc_personal_cargo','vc_personal_dependencia','mo_personal_remuneraciones','mo_personal_honorarios','mo_personal_incentivo','mo_personal_gratificacion','mo_personal_otros_beneficios','mo_personal_total','vc_personal_observaciones','fec_reg']
-            self.data_encontrada_infogeneral = pd.json_normalize(dict(item), ['personas'])
+            
+            self.data_encontrada_infogeneral = pd.json_normalize(item['personas'])
+            self.data_encontrada_infogeneral['tipo_poder_id'] = item['tipo_poder_id']
+            self.data_encontrada_infogeneral['tipo_poder_nombre'] = item['tipo_poder_nombre']
+            self.data_encontrada_infogeneral['categoria'] = item['categoria']
+            self.data_encontrada_infogeneral['entidad_id'] = item['entidad_id']
+            self.data_encontrada_infogeneral['entidad_nombre'] = item['entidad_nombre']
+
+            print(self.data_encontrada_infogeneral)
+            
             self.items_written_infogeneral += 1
             if self.items_written_infogeneral % self.ctd_save_infogeneral == 0 :
                 self.data_encontrada_infogeneral = self.guarda_data(self.data_encontrada_infogeneral, self.items_written_infogeneral, self.prename_infogeneral)
             return item
 
     def guarda_data(self, df_gral, ctd_items=0, prename=''):
-        df_gral.to_csv(self.prename_infogeneral, sep='\t',header=ctd_items<=self.ctd_save_infogeneral,index=False, encoding="utf-8", columns=self.columnsPrincipal, mode='a')
+        df_gral.to_csv(self.prename_infogeneral, sep='\t', header=ctd_items<=self.ctd_save_infogeneral, index=False, encoding="utf-8", columns=self.columnsPrincipal, mode='a')
         return None
 
     def __del__(self):
